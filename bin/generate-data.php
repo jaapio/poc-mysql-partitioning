31eee605ebb21bafaa41e294601028baf9005138
Jaapio <jaap@ijaap.nl>
2019-08-31 20:31:18 +0200

    UUID partitioning

--- bin/generate-data.php
+++ bin/generate-data.php
@@ -4,7 +4,8 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 try {
-    define('NUMBER_OF_ENTRIES', 10000);
+    define('NUMBER_OF_ENTRIES', 100);
+    define('NUMBER_OF_USERS', 10);
     $faker = Faker\Factory::create();
 
     $pdo = new PDO('mysql:host=db;dbname=test', 'root', 'root');
@@ -13,26 +14,33 @@ try {
     $result = $pdo->exec("
 CREATE TABLE test_partitioning (
     id BINARY(36) NOT NULL,
-    user_id INT NOT NULL,
+    user_id BINARY(36) NOT NULL,
     PRIMARY KEY(id, user_id)
 )
-PARTITION BY HASH(user_id)
+PARTITION BY KEY(user_id)
 PARTITIONS 10;"
-
     );
+
+    var_dump($pdo->errorInfo());
+    if ($result === false) {
+        exit(1);
+    }
 
     $statement = $pdo->prepare('insert into test_partitioning (id, user_id) values (:id, :user_id)');
 
-    for ($i = 1; $i <= NUMBER_OF_ENTRIES; $i++) {
-        $id     = $faker->uuid;
-        $userId = $faker->numberBetween();
+    for ($i = 1; $i < NUMBER_OF_USERS; $i++) {
+        $userId = $faker->uuid;
 
-        $statement->execute([
-            'id' => $id,
-            'user_id' => $userId,
-        ]);
-        echo $id . PHP_EOL;
+        for ($e = 1; $e <= NUMBER_OF_ENTRIES; $e++) {
+            $id = $faker->uuid;
+
+            $statement->execute([
+                'id' => $id,
+                'user_id' => $userId,
+            ]);
+            echo $id . PHP_EOL;
+        }
     }
-} catch (\Throwable $e) {
-    echo $e->getMessage();
+} catch (\Throwable $exception) {
+    echo $exception->getMessage();
 }
